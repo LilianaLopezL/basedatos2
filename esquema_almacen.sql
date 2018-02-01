912d6b382b587f2177f997d690504a9a84bd9427
LilianaLopezL <35473656+LilianaLopezL@users.noreply.github.com>
2018-01-31 20:41:08 -0600

    Update esquema_almacen.sql

--- esquema_almacen.sql
+++ esquema_almacen.sql
@@ -1,17 +1,17 @@
 
---GENERACIÓN DE LA PRIMER TABLA
+--GENERACIÃ“N DE LA PRIMER TABLA
 CREATE TABLE ALMACEN
 (
 NUMERO_ALMACEN INTEGER, 
 UBICACION_ALMACEN VARCHAR2(50),
 CONSTRAINT PK_NUM_ALM PRIMARY KEY (NUMERO_ALMACEN)
 );
--- Declaración de procedimiento
+-- DeclaraciÃ³n de procedimiento
 CREATE OR REPLACE PROCEDURE guardar_almacen(my_num_alm IN integer, my_ub_alm IN varchar2)
 AS
 -- Variables locales si se requieren
 BEGIN
---Cuerpo o lógica del procedimiento
+--Cuerpo o lÃ³gica del procedimiento
 INSERT INTO ALMACEN VALUES(my_num_alm,my_ub_alm);
 END;
 /
@@ -25,3 +25,4 @@ END;
 
 SELECT * FROM ALMACEN
 
+-- Probar el procedimiento con Netbeans
